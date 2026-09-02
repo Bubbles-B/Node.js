74a4e28f428104f12a65b1e0d70105d7dcca1d9a
Bubbles <sediiskaki@gmail.com>
2026-09-02 17:37:08 +0200

    Getting set up

---
+++ Unit_0/Lesson_2/messages.js
@@ -0,0 +1,18 @@
+//Listing 2.1 & 2.2
+"use strict"; // Enforces stricter parsing and error handling in JavaScript
+
+// Create an array of messages
+let messages = [
+    "A change of environment can be a good thing!", // Encouraging message about adaptability
+    "You will make it!",                            // Motivational message
+    "Just run with the code!"                       // Playful message encouraging action
+];
+
+// // Loop through the messages and log each one 
+// Listing 2.3
+// messages.forEach(m => console.log(m));
+
+console.g(messages[0])
+
+
+
